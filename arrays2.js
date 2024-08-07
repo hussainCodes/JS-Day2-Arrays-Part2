d5eb932bbf1f6a4e93b966f7a8c3f92eca1c86a9
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-08-07 09:45:21 +0300

    finish arrays2 tasks

---
+++ arrays2.js
@@ -0,0 +1,23 @@
+
+
+function isArrayLengthOdd(numbers){
+   if(numbers.length % 2 == 1){
+	return true;
+}else{
+	return false
+}
+}
+
+function isArrayLengthEven(numbers){
+   if(numbers.length % 2 == 0){
+        return true;
+}else{
+        return false
+}
+}
+
+function addLailaToArray(instructors){
+instructors.push("laila");
+}
+
+
