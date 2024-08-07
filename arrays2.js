fe2a6c039374ce2a37bda33a012c00ac671df0fe
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-08-07 10:48:54 +0300

    finish test for all tasks

--- arrays2.js
+++ arrays2.js
@@ -1,5 +1,7 @@
 
-
+let teams = ["Brazil", "Germany", "Italy"];
+let fruits = ["apple", "orange", "banana", "kiwi", "strawberry"];
+let instructors = ["Mshary", "Hasan"];
 function isArrayLengthOdd(numbers){
    if(numbers.length % 2 == 1){
 	return true;
@@ -17,7 +19,33 @@ function isArrayLengthEven(numbers){
 }
 
 function addLailaToArray(instructors){
-instructors.push("laila");
+  instructors.push("laila");
+  return instructors;
 }
+
+function eleminateTeam(teams){
+        
+    return teams.pop();
+}
+
+function secondHalfOfArrayIfItIsEven(fruits){
+        if(fruits.length % 2 == 0){
+           return fruits.slice(fruits.length/2, fruits.length);    
+        }else{
+           return [];
+        }
+}
+
+function youGottaCalmDown(shout){
+        let position = shout.indexOf("!");
+       if(position == -1 || position == shout.length){
+        return shout;
+       }
+        else{
+                return shout.slice(0, position +1);
+        }
+}
+
+console.log(isArrayLengthOdd(teams))
 
 
